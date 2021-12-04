a5295daa33c0f36538f21a209b31684db1f0eb06
Caltinor <62700786+Caltinor@users.noreply.github.com>
2021-12-03 19:02:34 -0500

    1.18 port

--- src/main/java/com/dicemc/tieredarmor/TieredArmor.java
+++ src/main/java/com/dicemc/tieredarmor/TieredArmor.java
@@ -12,9 +12,9 @@ import net.minecraft.world.item.CreativeModeTab;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.fmllegacy.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 @Mod(TieredArmor.MOD_ID)
 public class TieredArmor {
